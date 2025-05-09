db5e280a4d5fb1d44c6458a616fba7b6129ad294
totoogg <goncharov.vova2014@gmail.com>
2025-05-09 22:54:44 +0300

    feat: add webpack and TypeScript

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,8 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack, { web } from "webpack";
 
-module.exports = {
+const config: webpack.Configuration = {
   mode: "development",
   entry: path.resolve(__dirname, "src", "index.ts"),
   output: {
@@ -29,3 +29,5 @@ module.exports = {
     new webpack.ProgressPlugin(),
   ],
 };
+
+export default config;
