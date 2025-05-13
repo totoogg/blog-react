b46c941ff22a51d238470c8ddf9f9e754182c0c8
totoogg <goncharov.vova2014@gmail.com>
2025-05-13 23:22:08 +0300

    feat: add tests for button and sidebar components

---
+++ src/shared/lib/tests/renderWithTranslation/renderWithTranslation.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import { ReactNode } from "react";
+import { I18nextProvider } from "react-i18next";
+import i18ForTests from "shared/config/i18n/i18ForTests";
+
+export default function renderWithTranslation(component: ReactNode) {
+  return render(
+    <I18nextProvider i18n={i18ForTests}>{component}</I18nextProvider>
+  );
+}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import cls from "./Sidebar.module.scss";
 import { FC, useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
+import { Button } from "shared/ui/Button/Button";
 
 interface SidebarProps {
   className?: string;
@@ -17,11 +18,14 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
 
   return (
     <div
+      data-testid="sidebar"
       className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [
         className,
       ])}
     >
-      <button onClick={onToggle}>toggle</button>
+      <Button data-testid="sidebar-toggle" onClick={onToggle}>
+        toggle
+      </Button>
       <div className={cls.switcher}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
