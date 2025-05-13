8daf0613cafc2878caf204dff2048e1133aa110b
totoogg <goncharov.vova2014@gmail.com>
2025-05-13 14:47:05 +0300

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["./node_modules/*", "./build/*"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
+]);
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 
 export default function MainPage() {
-  const { t } = useTranslation('main');
+  const { t } = useTranslation("main");
 
   return <div>{t("main")}</div>;
 }
