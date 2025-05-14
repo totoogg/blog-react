2a7629b6a5cc60b316d396a84faf7542f5d5a3eb
totoogg <goncharov.vova2014@gmail.com>
2025-05-14 21:19:21 +0300

    fix: linting

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,12 +1,15 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import csl from "./Navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+
   return (
     <div className={classNames(csl.navbar, {}, [className])}>
       <div className={csl.links}>
@@ -15,10 +18,10 @@ export const Navbar = ({ className }: NavbarProps) => {
           to={"/"}
           className={csl["main-link"]}
         >
-          Main
+          {t("main")}
         </AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={"/about"}>
-          About
+          {t("about")}
         </AppLink>
       </div>
     </div>
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,12 +4,14 @@ import { FC, useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 import { Button } from "shared/ui/Button/Button";
+import { useTranslation } from "react-i18next";
 
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar: FC<SidebarProps> = ({ className }) => {
+  const { t } = useTranslation();
   const [collapsed, setCollapsed] = useState(false);
 
   const onToggle = () => {
@@ -24,7 +26,7 @@ export const Sidebar: FC<SidebarProps> = ({ className }) => {
       ])}
     >
       <Button data-testid="sidebar-toggle" onClick={onToggle}>
-        toggle
+        {t('toggle')}
       </Button>
       <div className={cls.switcher}>
         <ThemeSwitcher />
