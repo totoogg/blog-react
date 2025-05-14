678dfb709ce6e631dfe055ccc07db8bd877abe8f
totoogg <goncharov.vova2014@gmail.com>
2025-05-14 21:24:58 +0300

    fix: linting css

--- src/widgets/ErrorPage/ui/ErrorPage.tsx
+++ src/widgets/ErrorPage/ui/ErrorPage.tsx
@@ -1,7 +1,7 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { FC } from "react";
-import cls from "./ErrorPage.module.scss";
 import { useTranslation } from "react-i18next";
+import cls from "./ErrorPage.module.scss";
 
 interface ErrorPageProps {
   className?: string;
