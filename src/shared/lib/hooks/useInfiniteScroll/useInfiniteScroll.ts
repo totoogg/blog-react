116a9ca59cf6a1b20468c3d0fddd58c03f23d824
totoogg <goncharov.vova2014@gmail.com>
2025-05-30 00:25:26 +0300

    fix: closure error

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -12,11 +12,13 @@ export function useInfiniteScroll({
   wrapperRef,
 }: UseInfiniteScrollOptions) {
   useEffect(() => {
+    const wrapperElement = wrapperRef.current;
+    const triggerElement = triggerRef.current;
     let observer: IntersectionObserver;
 
     if (callback) {
       const options = {
-        root: wrapperRef.current,
+        root: wrapperElement,
         rootMargin: "0px",
         threshold: 1.0,
       };
@@ -27,13 +29,12 @@ export function useInfiniteScroll({
         }
       }, options);
 
-      observer.observe(triggerRef.current);
+      observer.observe(triggerElement);
     }
 
     return () => {
-      if (observer) {
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        observer.unobserve(triggerRef.current);
+      if (observer && triggerElement) {
+        observer.unobserve(triggerElement);
       }
     };
   }, [callback, triggerRef, wrapperRef]);
