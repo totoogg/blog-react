f0c406730dd11942b59632e78cdb39db759207b4
totoogg <goncharov.vova2014@gmail.com>
2025-06-21 13:05:15 +0300

    feat: add circular dependecy plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
 import webpack from "webpack";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import CopyPlugin from "copy-webpack-plugin";
+import CircularDependencyPlugin from "circular-dependency-plugin";
 import { BuildOptions } from "./types/config";
 
 export function buildPlugins({
@@ -28,6 +29,10 @@ export function buildPlugins({
     }),
     new CopyPlugin({
       patterns: [{ from: paths.locales, to: paths.buildLocales }],
+    }),
+    new CircularDependencyPlugin({
+      exclude: /node_modules/,
+      failOnError: true,
     }),
   ];
 
