b557770816edb80e680c8c2a636a91e397752311
totoogg <goncharov.vova2014@gmail.com>
2025-06-22 14:25:22 +0300

    fix: drawer

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -8,7 +8,6 @@ import { NotificationList } from "@/entities/Notification";
 import NotificationIcon from "@/shared/assets/icons/notification-20-20.svg";
 import { Drawer } from "@/shared/ui/Drawer/Drawer";
 import { BrowserView, MobileView } from "react-device-detect";
-import { AnimationProvider } from "@/shared/lib/components/AnimationProvider";
 
 interface NotificationButtonProps {
   className?: string;
@@ -45,11 +44,10 @@ export const NotificationButton: FC<NotificationButtonProps> = memo(
         </BrowserView>
         <MobileView>
           {trigger}
-          <AnimationProvider>
-            <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-              <NotificationList />
-            </Drawer>
-          </AnimationProvider>
+
+          <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+            <NotificationList />
+          </Drawer>
         </MobileView>
       </div>
     );
--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -4,7 +4,10 @@ import { useTheme } from "@/app/providers/ThemeProvider";
 import { Overlay } from "../Overlay/Overlay";
 import cls from "./Drawer.module.scss";
 import { Portal } from "../Portal/Portal";
-import { useAnimationLibs } from "@/shared/lib/components/AnimationProvider";
+import {
+  AnimationProvider,
+  useAnimationLibs,
+} from "@/shared/lib/components/AnimationProvider";
 
 interface DrawerProps {
   className?: string;
@@ -99,7 +102,7 @@ export const DrawerContent = memo((props: DrawerProps) => {
 
 DrawerContent.displayName = "DrawerContent";
 
-export const Drawer = memo((props: DrawerProps) => {
+const DrawerAsync = (props: DrawerProps) => {
   const { isLoaded } = useAnimationLibs();
 
   if (!isLoaded) {
@@ -107,6 +110,14 @@ export const Drawer = memo((props: DrawerProps) => {
   }
 
   return <DrawerContent {...props} />;
-});
+};
+
+export const Drawer = (props: DrawerProps) => {
+  return (
+    <AnimationProvider>
+      <DrawerAsync {...props} />
+    </AnimationProvider>
+  );
+};
 
 Drawer.displayName = "Drawer";
