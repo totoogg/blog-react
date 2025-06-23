89f0bc1b384b83d94fc10d39938aa6e4c52c6f68
totoogg <goncharov.vova2014@gmail.com>
2025-06-23 20:54:14 +0300

    fix: storybook

--- config/storybook/preview.tsx
+++ config/storybook/preview.tsx
@@ -79,6 +79,19 @@ const preview: Preview = {
       },
     },
   },
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    layout: "fullscreen",
+    msw: {
+      handlers: [],
+    },
+  },
   loaders: [mswLoader],
 };
 
