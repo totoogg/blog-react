0e0135a1abf7236469863cd5504b8bcfb60127f4
totoogg <goncharov.vova2014@gmail.com>
2025-06-25 14:52:52 +0300

    feat: add tests for isolated components with cypress

--- cypress/support/commands/common.ts
+++ cypress/support/commands/common.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { USER_LOCALSTORAGE_KEY } from "../../../src/shared/const/localStorage";
 import { User } from "../../../src/entities/User";
-import { selectByTestId } from "cypress/helpers/selectByTestId";
+import { selectByTestId } from "../../helpers/selectByTestId";
 
 export const login = (
   username: string = "testuser",
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable blog2Plugin/layer-imports */
 import { render, RenderResult } from "@testing-library/react";
 import { StateSchema, StoreProvider } from "@/app/providers/StoreProvider";
 import { ReactNode } from "react";
@@ -6,27 +7,50 @@ import { MemoryRouter } from "react-router-dom";
 import i18ForTests from "@/shared/config/i18n/i18ForTests";
 import { DeepPartial } from "../../../lib/deepPartial/deepPartial";
 import { ReducersMapObject } from "@reduxjs/toolkit";
+import { ThemeProvider } from "@/app/providers/ThemeProvider";
+import { Theme } from "@/shared/const/theme";
+import "@/app/styles/index.scss";
 
 export interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
+}
+
+interface TestProviderProps {
+  children: ReactNode;
+  options?: ComponentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const { children, options = {} } = props;
+  const {
+    route = "/",
+    initialState,
+    asyncReducers,
+    theme = Theme.LIGHT,
+  } = options;
+
+  return (
+    <MemoryRouter initialEntries={[route]}>
+      <StoreProvider
+        asyncReducers={asyncReducers}
+        initialState={initialState as StateSchema}
+      >
+        <I18nextProvider i18n={i18ForTests}>
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>{children}</div>
+          </ThemeProvider>
+        </I18nextProvider>
+      </StoreProvider>
+    </MemoryRouter>
+  );
 }
 
 export default function componentRender(
   component: ReactNode,
   options: ComponentRenderOptions = {}
 ): RenderResult {
-  const { route = "/", initialState, asyncReducers } = options;
-
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <StoreProvider
-        asyncReducers={asyncReducers}
-        initialState={initialState as StateSchema}
-      >
-        <I18nextProvider i18n={i18ForTests}>{component}</I18nextProvider>
-      </StoreProvider>
-    </MemoryRouter>
-  );
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 }
